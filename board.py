d4bf7b242d9baff0d81d605b9eb9973021435514
Octavio perez <octavio.perez@iteso.mx>
2024-01-28 01:25:43 -0600

    Added turn and round logic

--- board.py
+++ board.py
@@ -1,30 +1,86 @@
 import numpy 
 import random
+from typing import List
+from utils.__init__ import getSquaresInfo, getChanceCards
 
 
 class Player():
 
     def __init__(self, playerNumber: int, properties: list[object]) -> None:
-        self.playerNumber = playerNumber
-        
+        self.playerNumber = playerNumber      
         self.properties = properties
 
-    initialMoney = 200 #All players have the same money
+    money = 200 #All players have the same money when starting
+    position = 0
+    jail = False
+
+
 
 class Monopoly():
-
-    def __init__(self, players: list[object]) -> None:
+   
+    def __init__(self, players: List[object]) -> None:
         self.players = players
 
-
-    def roll_dice() -> int:
-        return random.randint(1, 6)
+    rounds = 1
+    squares = getSquaresInfo()
+    chanceCards = getChanceCards()
 
     
-    throws = 0
+    def roll_dice(self) -> int:
+        return random.randint(1, 6)
 
-class Board():
-    pass
+
+    def turn(self,player: object):
+
+        if player.jail == True:
+            print(f"Player{player.playerNumber} in jail")
+            player.jail = False
+        
+        else:
+            #Roll dice
+            dice = self.roll_dice() #why do i need to use self to call function inside the same class?
+        
+            lastPosition = player.position #Last position
+            player.position = (player.position + dice) % 40 #New position
+
+            #Give player $200 if they complete a round
+            if player.position < lastPosition:
+                player.money +=  200
+
+            print(f"Player {player.playerNumber} - Dice {dice} - Last Position {lastPosition} - Position {player.position} - Money {player.money}")
+
+            #Jail ------------------------------->
+            if player.position == 10:
+                player.jail = True
+
+            elif player.position == 30:
+                player.jail = True
+                player.position = 10
+
+            #Chance Cards ----------------------->
+            if player.position in (2,7,17,22,33,36):
+                print("Chance carrd or chest")
+                player.money += random.choice(self.chanceCards)
+
+            #Taxes ------------------------------>
+
+            #Mutate properties information
+
+
+            #Mutate position and money of player
+            ...
+
+    def round(self,players: List[object]):
+        #Make a turn for all players
+        #print(f"Round: {self.rounds}")
+        for p in players:
+            self.turn(p)
+        self.rounds += 1
+
+
+
+
+    
 
 def main(n_players):
 
@@ -34,16 +90,26 @@ def main(n_players):
     for i in range(n_players):
         players.append(Player(i,[]))
 
+    #Monopoly creating
     game = Monopoly(players)
 
 
+    #Games goes on until someone gets out of money
+    playerWithLessMoney = min([p.money for p in game.players])
+    
+    #while playerWithLessMoney > 0:
+    for i in range(200):
+
+        game.round(players)
+        playerWithLessMoney = min([p.money for p in game.players])
 
 
-
-
-
+    
 
 
 if __name__ == '__main__':
-    main(4)
+    main(1)
+
+
+
 
