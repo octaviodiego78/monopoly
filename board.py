bf70480e8fdcff672fa0c9650baf73b9d2d0580f
Octavio perez <octavio.perez@iteso.mx>
2024-02-02 22:12:05 -0600

    Added logic por property purchases, house additions, money updates and rent prices updates

--- board.py
+++ board.py
@@ -3,6 +3,11 @@ import random
 from typing import List
 from utils.__init__ import getSquaresInfo, getChanceCards
 
+"""Move money, position, and jail attributes into _init_ in the Player class: These should be instance attributes, not class attributes. Class attributes are shared across all instances of the class, which isn't what you want for individual players.
+Change Properties class name to Property: It's a common practice to use singular names for classes.
+Integrate the Property class with the Monopoly class: You mentioned that Property does not exist inside the Monopoly class, but you want to relate it to the players. We can create a list of properties as a class attribute in Monopoly and assign owners as Player objects.
+Correcting turn and round methods: I'll adjust the methods to make them more consistent and efficient."""
+
 
 class Player():
 
@@ -10,11 +15,11 @@ class Player():
         self.playerNumber = playerNumber      
         self.properties = properties
 
-    money = 2500 #All players have the same money when starting
-    position = 0
-    jail = False
+        self.money = 2500  #All players have the same money when starting
+        self.position = 0
+        self.jail = False
 
-class Properties():
+class Property():
     def __init__(self, **kwargs) -> None:
         for k,v in kwargs.items():
             setattr(self, k, v)
@@ -24,19 +29,31 @@ class Monopoly():
     def __init__(self, players: List[object]) -> None:
         self.players = players
 
+    class Property():
+     def __init__(self, **kwargs) -> None:
+        for k,v in kwargs.items():
+            setattr(self, k, v)
+
     rounds = 1
     chanceCards = getChanceCards()
-    properties = [Properties(**pm) for pm in getSquaresInfo()]
-
-    print(vars(properties[0]))
-
-
     
+    properties = [Property(**pm) for pm in getSquaresInfo()] #Create a list of Property objects
+                                                                #Each object recieves a dict and create a Property object setting the k,v of the dict 
+                                                                # as attributes of the said object
+ 
+    print(properties[0].owner)
+
     def roll_dice(self) -> int:
         return random.randint(1, 6)
+    
+    def binaryChoice():
+        return random.choice([True, False])
+    
+    def housesChoice(n):
+        return random.randint(1, n)
 
 
-    def turn(self,player: object):
+    def turn(self,player: Player):
 
         if player.jail == True:
             print(f"Player{player.playerNumber} in jail")
@@ -44,7 +61,7 @@ class Monopoly():
         
         else:
             #Roll dice
-            dice = self.roll_dice() #why do i need to use self to call function inside the same class?
+            dice = self.roll_dice()
         
             lastPosition = player.position #Last position
             player.position = (player.position + dice) % 40 #New position
@@ -53,7 +70,7 @@ class Monopoly():
             if player.position < lastPosition:
                 player.money +=  200
 
-            print(f"Player {player.playerNumber} - Dice {dice} - Last Position {lastPosition} - Position {player.position} - Money {player.money}")
+            #print(f"Player {player.playerNumber} - Dice {dice} - Last Position {lastPosition} - Position {player.position} - Money {player.money}")
 
             #Jail ------------------------------->
             if player.position == 10:
@@ -86,7 +103,35 @@ class Monopoly():
 
 
             #Mutate position and money of player
-            ...
+            currentProperty = self.properties[player.currentPosition] #does properties need a self because it's created inside a method from monopoly class?
+
+            if currentProperty.owner == player: 
+                #Simulating the player decision to add a house or many  to the property
+                    #For properties with less than 5 house
+                    #Only is your simulated descition to buy a house is True
+                    #If you have enough money for houses
+                propertyHouses = currentProperty.houses
+                if (5 - propertyHouses) > 0 and (player.money > ((5 - propertyHouses)*currentProperty.costPerHouse)) and self.binaryChoice: 
+
+                    housesToAdd = self.housesChoice(5 - propertyHouses) #Decide houses to add
+                    currentProperty.houses += housesToAdd               #Update amount of houses on property
+                    player.money -= currentProperty.costPerHouse * housesToAdd  #Pay for the houses
+                    currentProperty.rent = currentProperty.rentWithHouses[currentProperty.houses -1]    #Set rent to the price with n houses
+
+            else:
+                if currentProperty.owner == None:
+                    if self.binaryChoice():
+                        player.money -=  currentProperty.price #Pay for the property 
+                        currentProperty.owner = player #Update property owner
+
+                else:
+                    #Paying rent to the owner
+                    player.money -= currentProperty.rent
+                    currentProperty.owner.money += currentProperty.rent
+                    
+    
+
+
 
     def round(self,players: List[object]):
         #Make a turn for all players
@@ -110,6 +155,7 @@ def main(n_players):
 
     #Monopoly creating
     game = Monopoly(players)
+
 
 
     #Games goes on until someone gets out of money
