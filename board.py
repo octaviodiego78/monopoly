5568db687645067a920ba108fa1c788fce995829
Octavio perez <octavio.perez@iteso.mx>
2024-03-24 16:09:54 -0600

    Added logic to create Q tables

--- board.py
+++ board.py
@@ -5,9 +5,7 @@ from utils.__init__ import getSquaresInfo, getChanceCards
 import pandas as pd
 
 
-# Possible rent to pay
-# Rent to charge 
-# Houses
+
 
 
 
@@ -16,8 +14,8 @@ class Player():
     def __init__(self, playerNumber: int, properties: list[object]) -> None:
         self.playerNumber = playerNumber      
         self.properties = properties
-
-        self.money = 2500  #All players have the same money when starting
+        self.money = 2500  
+        self.land_value = 0
         self.position = 0
         self.jail = False
         self.railroads = 0
@@ -33,7 +31,32 @@ class Monopoly():
    
     def __init__(self, players: List[Player]) -> None:
         self.players = players
-        self.results = pd.DataFrame(columns = ["player","current_position", "dice","position","jail","is_special_position","money", "properties", "networth" ])
+        self.results = pd.DataFrame(
+            columns = ["player_number"
+                        ,"last_position"
+                        ,"dice"
+                        ,"current_position"
+                        ,"jail"
+                        ,"is_special_position"
+                        ,"money"
+                        ,"properties"
+                        ,"networth"
+                        ,"owned_properties_price"
+                        ,"owned_properties_houses"
+                        ,"rent_to_charge"
+                        ,"percentage_of_owned_properties"
+                        ,"railroads_owned"
+                        ,"properties_available"
+                        ,"buy"
+                        ,"money2"
+                        ,"properties2"
+                        ,"networth2"
+                        ,"owned_properties_price2"
+                        ,"owned_properties_houses2"
+                        ,"rent_to_charge2"
+                        ,"percentage_of_owned_properties2"
+                        ,"railroads_owned2"
+                        ,"properties_available2"])
 
 
     rounds = 1
@@ -56,26 +79,60 @@ class Monopoly():
 
     def turn(self,player: Player):
 
+        choice = self.binaryChoice() 
+        lastPosition = player.position
+
+
         if player.jail == True:
             player.jail = False
 
             dice = 0
-            lastPosition = player.position #Last position
             player.position = (player.position + dice) % 40 #New position
-        
+            qt = [player.playerNumber #Player number
+                ,lastPosition #Last position
+                , dice #Dice
+                , player.position #New position
+                , player.jail #Jail
+                , self.properties[player.position].isSpecialSquare #Special position
+                , player.money #Money
+                , len(player.properties) #Number of properties owned
+                , player.money + sum([p.price for p in player.properties]) #networth
+                , sum([p.price for p in player.properties]) #Owned Properties price 
+                , sum([p.houses for p in player.properties]) #Owned properties Houses
+                , sum([p.rent for p in player.properties]) #Rent to charge
+                , round(len(player.properties) / 28,2) #Percentage of properties owned
+                , player.railroads #Railroads owned
+                , round(len([p for p in self.properties if p.owner == None and p.isSpecialSquare == False]) / 27,2) #Percentage of properties avaible 
+                ]
+
         else:
             #Roll dice
-            dice = self.roll_dice()
-        
-            lastPosition = player.position #Last position
+            dice = self.roll_dice()      
             player.position = (player.position + dice) % 40 #New position
+
+            qt = [player.playerNumber #Player number
+                ,lastPosition #Last position
+                , dice #Dice
+                , player.position #New position
+                , player.jail #Jail
+                , self.properties[player.position].isSpecialSquare #Special position
+                , player.money #Money
+                , len(player.properties) #Number of properties owned
+                , player.money + sum([p.price for p in player.properties]) #networth
+                , sum([p.price for p in player.properties]) #Owned Properties price 
+                , sum([p.houses for p in player.properties]) #Owned properties Houses
+                , sum([p.rent for p in player.properties]) #Rent to charge
+                , round(len(player.properties) / 28,2) #Percentage of properties owned
+                , player.railroads #Railroads owned
+                , round(len([p for p in self.properties if p.owner == None and p.isSpecialSquare == False]) / 27,2) #Percentage of properties avaible 
+                ]
 
             #Give player $200 if they complete a round
             if player.position < lastPosition:
                 player.money +=  200
 
 
-            if self.properties[player.position].isSpecialSquare == True:
+            if self.properties[player.position].isSpecialSquare == True: #Special squares logic
                 #Jail ------------------------------->
                 if player.position == 10:
                     player.jail = True
@@ -100,21 +157,21 @@ class Monopoly():
 
             else:
 
-                #Mutate position and money of player
+                #Mutate position of player
                 currentProperty = self.properties[player.position] 
 
                 if player.position in (5,15,25,35): #If position is a railroad
                     
                     if currentProperty.owner != player: #If the owner of the railroad is not the player
                         if currentProperty.owner == None:  #if no one is the owner
-                            if player.money > currentProperty.price and self.binaryChoice(): #option to buy it
+                            if player.money > currentProperty.price and choice: 
                                 currentProperty.owner = player
                                 player.money -= currentProperty.price
                                 player.railroads += 1
                         else:
                             rentToPay =  currentProperty.owner.railroads*50
-                            currentProperty.owner += rentToPay
-                            player -= rentToPay
+                            currentProperty.owner.money += rentToPay
+                            player.money -= rentToPay
                 
                 else:
                     if currentProperty.owner == player: #Simulating the player decision to add a house or many  to the property    
@@ -122,30 +179,39 @@ class Monopoly():
                             #Only is your simulated descition to buy a house is True
                             #If you have enough money for houses
                         propertyHouses = currentProperty.houses
-                        if (5 - propertyHouses) > 0 and (player.money > ((5 - propertyHouses)*currentProperty.costPerHouse)) and self.binaryChoice: 
+                        if (5 - propertyHouses) > 0 and (player.money > currentProperty.costPerHouse) and choice: 
 
-                            housesToAdd = self.housesChoice(5 - propertyHouses) #Decide houses to add
-                            currentProperty.houses += housesToAdd               #Update amount of houses on property
-                            player.money -= currentProperty.costPerHouse * housesToAdd  #Pay for the houses
+                            
+                            currentProperty.houses += 1               #Update amount of houses on property
+                            player.money -= currentProperty.costPerHouse  #Pay for the houses
                             currentProperty.rent = currentProperty.rentWithHouses[currentProperty.houses -1]    #Set rent to the price with n houses
 
-                    else: #buying the property
-                        if currentProperty.owner == None:   
-                            if self.binaryChoice(): 
+                    else: 
+                        if currentProperty.owner == None:  #buying the property
+                            if choice: 
                                 player.money -=  currentProperty.price #Pay for the property 
                                 currentProperty.owner = player #Update property owner
                                 player.properties.append(currentProperty) #Add property to player's properties
 
 
-                        else:
-                            #Paying rent to the owner
+                        else: #Paying rent to the owner
                             rentToPay = currentProperty.rent
                             player.money -= rentToPay
                             currentProperty.owner.money += rentToPay
     
-        return [player.playerNumber,lastPosition, dice, player.position, player.jail
-                , self.properties[player.position].isSpecialSquare, player.money, len(player.properties)
-                , player.money + sum([p.price for p in player.properties])] 
+        qt1 = [
+                 player.money #Money
+                , len(player.properties) #Number of properties
+                , player.money + sum([p.price for p in player.properties]) #networth
+                , sum([p.price for p in player.properties]) #Owned Properties price 
+                , sum([p.houses for p in player.properties]) # Owned Houses
+                , sum([p.rent for p in player.properties]) #Rent to charge
+                , round(len(player.properties) / 28,2) #Percentage of properties owned
+                , player.railroads #Railroads owned
+                , round(len([p for p in self.properties if p.owner == None and p.isSpecialSquare == False]) / 27,2) #Percentage of properties avaible 
+                ]
+  
+        return qt + [choice] + qt1
 
     def round(self,players: List[Player]):
         #Make a turn for all players
@@ -171,6 +237,7 @@ def main(n_players):
 
         monopoly.round(players)
 
+    #monopoly.results.loc[monopoly.results["is_special_position"] == False] .to_csv("results.csv")
     monopoly.results.to_csv("results.csv")
 
     
